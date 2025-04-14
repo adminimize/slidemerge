557beb8c1be88a33980da0ff726d6d39df5204df
Cameron Slipp <cdslipp@gmail.com>
2025-04-14 15:41:05 -0400

    adding vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import '../app.css';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
 	import { createEventDispatcher } from 'svelte';
 	
 	// Add page-level drag and drop handling
